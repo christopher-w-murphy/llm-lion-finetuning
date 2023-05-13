ac2b36e150b7cd1e84893f8c6d324899324ad529
christopher-w-murphy <murphtron5000@gmail.com>
2023-05-13 11:32:17 -0400

    created function to summarize trainable params

--- src/domain/model.py
+++ src/domain/model.py
@@ -72,3 +72,21 @@ def get_trainer(
         data_collator=data_collator,
         train_dataset=train_dataset,
     )
+
+
+def summarize_trainable_parameters(model: PeftModel) -> str:
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        num_params = param.numel()
+        # if using DS Zero 3 and the weights are initialized empty
+        if num_params == 0 and hasattr(param, "ds_numel"):
+            num_params = param.ds_numel
+
+        all_param += num_params
+        if param.requires_grad:
+            trainable_params += num_params
+    return f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
