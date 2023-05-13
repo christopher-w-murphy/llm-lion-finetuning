1a372cd6c26158c135346a5dd8b28f62c6b8d9b7
christopher-w-murphy <murphtron5000@gmail.com>
2023-05-13 13:46:30 -0400

    run all 3 steps with 1 click

--- app.py
+++ app.py
@@ -12,28 +12,26 @@ def main():
     A simple Streamlit app to train and evaluate the models.
     """
     title("Fine-Tune an LLM with Lion.")
+
     model_size = selectbox("Select FLAN-T5 model size.", ["Small", "XXL"], help="Fine-tune a 80M param model for 1 epoch, or fine-tune a 11B param model for 5 epochs.")
 
-    header("Step 1. Load and prepare the Samsum dataset")
-    load_and_prepare_dataset = button("Load and Prepare Dataset")
-    if load_and_prepare_dataset:
+    run = button("Run")
+    if run:
+
+        header("Step 1. Load and prepare the Samsum dataset")
         step1_start = time()
         app_prepare_dataset(model_size)
         step1_end = time()
         write(f"Step 1 took {step1_end - step1_start:.1f} seconds.")
 
-    header("Step 2. Fine-Tune T5 with LoRA and bnb int-8")
-    fine_tine_t5 = button("Fine-Tune T5")
-    if fine_tine_t5:
+        header("Step 2. Fine-Tune T5 with LoRA and bnb int-8")
         step2_start = time()
         app_fine_tune(model_size)
         step2_end = time()
         step2_time_diff = (step2_end - step2_start) / 3600
         write(f"Step 2 took {step2_time_diff:.2f} hours.")
 
-    header("Step 3. Evaluate & run Inference with LoRA FLAN-T5")
-    evaluate_model = button("Evaluate Model")
-    if evaluate_model:
+        header("Step 3. Evaluate & run Inference with LoRA FLAN-T5")
         step3_start = time()
         app_evaluate_lora_model(model_size)
         step3_end = time()
