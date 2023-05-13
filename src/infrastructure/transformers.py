f6feeafd344c4ad6a942d1dfb301d153bd587841
christopher-w-murphy <murphtron5000@gmail.com>
2023-05-13 13:48:26 -0400

    load in 8-bit

--- src/infrastructure/transformers.py
+++ src/infrastructure/transformers.py
@@ -7,14 +7,13 @@ def load_tokenizer(tokenizer_id: str) -> PreTrainedTokenizer:
 
 
 def load_base_model(base_model_id: str) -> PreTrainedModel:
-    # return AutoModelForSeq2SeqLM.from_pretrained(base_model_id, load_in_8bit=True, device_map="auto")
-    return AutoModelForSeq2SeqLM.from_pretrained(base_model_id, device_map="auto")
+    return AutoModelForSeq2SeqLM.from_pretrained(base_model_id, load_in_8bit=True, device_map="auto")
 
 
 def load_model_from_config(config: PeftConfig) -> PreTrainedModel:
     return AutoModelForSeq2SeqLM.from_pretrained(
         config.base_model_name_or_path,
-        # load_in_8bit=True,
+        load_in_8bit=True,
         device_map={"": 0}
     )
 
