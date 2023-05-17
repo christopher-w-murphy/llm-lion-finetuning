3d0ff9dc915905818634463b4c40e7d0b39a5e03
christopher-w-murphy <murphtron5000@gmail.com>
2023-05-16 21:22:28 -0400

    updated handling of env vars

--- src/infrastructure/streamlit.py
+++ src/infrastructure/streamlit.py
@@ -1,7 +1,7 @@
 from os import getenv
 from typing import Dict, Any, Union, Optional
 
-from streamlit import secrets
+from streamlit import secrets, warning
 from streamlit.runtime.state import SessionStateProxy
 
 
@@ -9,6 +9,8 @@ ConfigType = Union[SessionStateProxy, Dict[str, Any]]
 
 
 def get_secret(secret_name: str) -> Optional[str]:
-    if secret_name in secrets:
+    try:
         return secrets[secret_name]
-    return getenv(secret_name)
+    except (FileNotFoundError, KeyError) as e:
+        warning(e)
+        return getenv(secret_name)
