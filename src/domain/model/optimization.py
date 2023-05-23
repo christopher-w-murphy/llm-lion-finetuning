6915430794743e8023f811fd54c76823aa280fe3
christopher-w-murphy <murphtron5000@gmail.com>
2023-05-23 18:17:02 -0400

    initialize Lion by only considering trainable parameters

--- src/domain/model/optimization.py
+++ src/domain/model/optimization.py
@@ -24,7 +24,8 @@ def get_optimizer(model: PeftModel, optim_name: str) -> Optimizer8bit:
     if optim_name.lower() == 'adamw':
         return AdamW8bit(model.parameters(), **adamw_hyperparameters)
     elif optim_name.lower() == 'lion':
-        return Lion8bit(model.parameters(), **lion_hyperparameters)
+        params = filter(lambda param: param.requires_grad, model.parameters())
+        return Lion8bit(params, **lion_hyperparameters)
     else:
         raise ValueError(f"Invalid optimizer name: {optim_name}.")
 
