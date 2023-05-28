e2c498e2e2c1f01e8161f1c0c11587718f9e5e91
christopher-w-murphy <murphtron5000@gmail.com>
2023-05-28 19:50:56 -0400

    removed dtype arg

--- src/infrastructure/transformers.py
+++ src/infrastructure/transformers.py
@@ -1,4 +1,3 @@
-from torch import float16
 from transformers import AutoTokenizer, PreTrainedTokenizer, AutoModelForSeq2SeqLM, PreTrainedModel
 
 
@@ -7,4 +6,4 @@ def load_tokenizer(tokenizer_id: str) -> PreTrainedTokenizer:
 
 
 def load_base_model(base_model_id: str) -> PreTrainedModel:
-    return AutoModelForSeq2SeqLM.from_pretrained(base_model_id, load_in_8bit=True, device_map="auto", torch_dtype=float16)
+    return AutoModelForSeq2SeqLM.from_pretrained(base_model_id, load_in_8bit=True, device_map="auto")
