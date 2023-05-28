f30d08d2acf222c43ebf0dbdcbe0657e1f5adc65
christopher-w-murphy <murphtron5000@gmail.com>
2023-05-28 17:34:04 -0400

    use learning rate from demo

--- src/domain/model/optimization.py
+++ src/domain/model/optimization.py
@@ -1,24 +1,24 @@
 from typing import Tuple
 
 from bitsandbytes.optim import Lion8bit
-from peft import PeftModel
+from peft import PeftModelForSeq2SeqLM
 from torch.optim import AdamW, Optimizer
 from torch.optim.lr_scheduler import LambdaLR
 from transformers.optimization import get_constant_schedule
 
 
 adamw_hyperparameters = {
-    'lr': 3e-5,
+    'lr': 1e-3,  # higher learning rate
     'betas': (0.9, 0.99)
 }
 
 lion_hyperparameters = {
-    'lr': 3e-6,
+    'lr': 1e-4,  # order of magnitude smaller than Adam
     'betas': (0.95, 0.98)
 }
 
 
-def get_optimizer(model: PeftModel, optim_name: str) -> Optimizer:
+def get_optimizer(model: PeftModelForSeq2SeqLM, optim_name: str) -> Optimizer:
     if optim_name.lower() == 'adamw':
         return AdamW(model.parameters(), **adamw_hyperparameters)
     elif optim_name.lower() == 'lion':
@@ -27,7 +27,7 @@ def get_optimizer(model: PeftModel, optim_name: str) -> Optimizer:
         raise ValueError(f"Invalid optimizer name: {optim_name}.")
 
 
-def get_optimizers(model: PeftModel, optim_name: str) -> Tuple[Optimizer, LambdaLR]:
+def get_optimizers(model: PeftModelForSeq2SeqLM, optim_name: str) -> Tuple[Optimizer, LambdaLR]:
     optimizer = get_optimizer(model, optim_name)
     lr_scheduler = get_constant_schedule(optimizer)
     return optimizer, lr_scheduler
