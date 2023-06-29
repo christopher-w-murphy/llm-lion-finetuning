a43084d2c6d12c9f6f55ef4fab6762c2c46b76a4
christopher-w-murphy <murphtron5000@gmail.com>
2023-06-28 21:46:11 -0400

    process new env var

--- src/infrastructure/huggingface_hub.py
+++ src/infrastructure/huggingface_hub.py
@@ -7,17 +7,11 @@ from typing import Optional, Dict, Any
 
 from huggingface_hub import HfApi
 
-from src.infrastructure.utilities import strtobool
-
 
 def get_huggingface_hub_connection(token: Optional[str] = None) -> HfApi:
     if token is None:
         token = getenv('HUGGINGFACE_TOKEN')
     return HfApi(token=token)
-
-
-def mock_saving() -> bool:
-    return getenv('MOCK_SAVING') is not None and strtobool(getenv('MOCK_SAVING'))
 
 
 def format_log_filename(upload_time: Optional[datetime] = None) -> str:
